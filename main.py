0e135059684a99ab551e05d78ad56a613aeb0027
shubhammgits <shubhamm18.work@gmail.com>
2025-10-01 20:32:14 +0530

    streamlit page config

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import os
 
 import streamlit as st
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+
+st.set_page_config(
+    page_title="Chat with Gemini",
+    page_icon="🤖",
+    layout="centered",
+    initial_sidebar_state="auto"
+)
+
